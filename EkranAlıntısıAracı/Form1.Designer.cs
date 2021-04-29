99bdb3d7dc184913d06b338789053d912e76bd19
Caner Ay Celep <72454642+caner24@users.noreply.github.com>
2021-04-30 00:57:44 +0300

    Add project files.

---
+++ EkranAlıntısıAracı/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace EkranAlıntısıAracı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.resmiKaydetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem9 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resmiKaydetToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.aToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1270, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // resmiKaydetToolStripMenuItem
+            // 
+            this.resmiKaydetToolStripMenuItem.Image = global::EkranAlıntısıAracı.Properties.Resources.pngwing_com;
+            this.resmiKaydetToolStripMenuItem.Name = "resmiKaydetToolStripMenuItem";
+            this.resmiKaydetToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.resmiKaydetToolStripMenuItem.Text = "                 ";
+            this.resmiKaydetToolStripMenuItem.Click += new System.EventHandler(this.resmiKaydetToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Image = global::EkranAlıntısıAracı.Properties.Resources.save_icon;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(83, 20);
+            this.toolStripMenuItem1.Text = "                ";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // aToolStripMenuItem
+            // 
+            this.aToolStripMenuItem.BackgroundImage = global::EkranAlıntısıAracı.Properties.Resources.kalem;
+            this.aToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.aToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem7,
+            this.toolStripMenuItem8,
+            this.toolStripMenuItem9});
+            this.aToolStripMenuItem.Name = "aToolStripMenuItem";
+            this.aToolStripMenuItem.Size = new System.Drawing.Size(34, 20);
+            this.aToolStripMenuItem.Text = "     ";
+            this.aToolStripMenuItem.Click += new System.EventHandler(this.aToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem4.Text = "3";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem5.Text = "5";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem6.Text = "8";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem6_Click);
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem7.Text = "10";
+            this.toolStripMenuItem7.Click += new System.EventHandler(this.toolStripMenuItem7_Click);
+            // 
+            // toolStripMenuItem8
+            // 
+            this.toolStripMenuItem8.Name = "toolStripMenuItem8";
+            this.toolStripMenuItem8.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem8.Text = "20";
+            this.toolStripMenuItem8.Click += new System.EventHandler(this.toolStripMenuItem8_Click);
+            // 
+            // toolStripMenuItem9
+            // 
+            this.toolStripMenuItem9.Name = "toolStripMenuItem9";
+            this.toolStripMenuItem9.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem9.Text = "30";
+            this.toolStripMenuItem9.Click += new System.EventHandler(this.toolStripMenuItem9_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1270, 561);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1270, 585);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem resmiKaydetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem aToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem8;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem9;
+    }
+}
+
---
+++ EkranAlıntısıAracı/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+
+namespace EkranAlıntısıAracı
+{
+    public partial class Form1 : Form
+    {
+
+        // ******************************************************** Developed by caner24 ********************************************************************* //
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Aldığımız ekran görüntüsünde herhangi bir çizme işlemi yapmak istersek diye çizme işlemi tanımlandı.
+        bool cizmeislemi;
+        int xkonum;  // çizme işlemini yapabilmemiz için x değişkeni oluşturuldu.
+        int ykonum;  // çizme işlemini yapabilmemiz için y değişkeni oluşturuldu.
+        int durum;  // çizme işlemini yapabilmemiz için x değişkeni oluşturuldu.
+        int kalinlik=5; // Çizme işlemi olduğu için kalem kalınlığı başlangıç değerini atadik.
+        Pen kalem; // Kalem nesnemizi oluşturduk 
+        Graphics gr; // Grafik nesnemizi oluşturduk
+
+        private void resmiKaydetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Çalıştırdığımız programı ekran görüntüsü alırken dışında tutması için hide özelliğinden yaralandık.
+            this.Hide();
+            // Bitmap nesnesi oluşturuldu, genişlik ve yüksekliği bilgisayar ekranımıza göre almasını sağladık.
+            Bitmap crp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            // grafik nesnemize işlem yapabilmesi için bitmap nesnemizi aktardık.
+            gr = Graphics.FromImage(crp);
+            // Grafik nesnemiz boyutları belirlenmiş olan görüntüyü almasını sağladık.
+            gr.CopyFromScreen(0, 0, 0, 0, new Size(crp.Width, crp.Height));
+            // Formumuza aldığımız ekran görüntüsünün görünmesi için BackgroundImage ' a aktardık.
+            panel1.BackgroundImage = crp;
+            // Tekrardan uygulamamızın görünmesiiçin show özelliğinden yararlandık.
+            this.Show();
+        }
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            // Kullanici aldığı ekran görüntüsünü kaydetmek isterse gerekli işlemler yapıldı.
+
+            // Kaydederken çıkan açıklama
+            saveFileDialog1.Title = "Kayit Edin";
+            // Kayıt edeceğimiz formatlar 
+            saveFileDialog1.Filter = "Resim Dosyasi(*.jpg) | *.jpg";
+            // Kullanıcı kaydet ' e basarsa olacak adımlar.
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // kaydedeceğimiz dosyanın yolunu ve adresini string değişkenimize aktarıyoruz.
+                string dosyaadi = saveFileDialog1.FileName;
+                // Bitmap nesnesi oluşturuldu, genişlik ve yüksekliği bilgisayar ekranımıza göre almasını sağladık.
+                Bitmap bmp = new Bitmap(panel1.Width, panel1.Height);
+                // grafik nesnemize işlem yapabilmesi için bitmap nesnemizi aktardık.
+                gr = Graphics.FromImage(bmp);
+                // Belirli bir alanın görüntüsünü alacığımz için Rectangle özelliklerinden yaralandık.
+                var cerceve = panel1.RectangleToScreen(panel1.ClientRectangle);
+                // Grafik nesnemiz boyutları belirlenmiş olan görüntüyü almasını sağladık.
+                gr.CopyFromScreen(cerceve.Location, Point.Empty, panel1.Size);
+                // Aldığımız yol sayesinde resmimizi ilgili yola kaydediyoruz.
+                bmp.Save(dosyaadi);
+            }
+        }
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Kalem sekmesine tıklandığı zaman ve mause hareket ediyorsa Çizme işlemini aktif ediyoruz.
+            cizmeislemi = true;
+            // Aktif konumları yani çizme işlemini yaparken mause imlecimizin sağa sola nereye gittiğini programın anlaması için e özelliğinden yararlandık.
+            xkonum = e.X;
+            ykonum = e.Y;
+        }
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Eğer mause basılı tutmuyorsak çizme işleminin olmaması için mouse up özelliğinden yararlanıp çizme işlemini false yaptık.
+            cizmeislemi = false;
+        }
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Eğer kalem sekmemize tıklanmışsa yani durum değişkenimize değer atanmışsa çizme işlemlerini yaptık.
+            if (durum == 1)
+            {
+                // Grafik nesnemizi oluşturduk.
+                gr = panel1.CreateGraphics();
+                //Grafik nesnemizin konumlarını belirledik.
+                Point konum1 = new Point(xkonum, ykonum);
+                Point konum2 = new Point(e.X, e.Y);
+                if (cizmeislemi == true)
+                {
+                    gr.DrawLine(kalem, konum1, konum2); // Kalem için Drawline özelliğimizden yararlandık böylece çizgi çizebileceğiz.
+                    xkonum = e.X; // sağa sola çizgi yapılırsa xkonum ve ykonum belirleniyor yani boyut ne kadar olacığı burada yazmasan başlangıç noktası etrafında uzun devam eder.
+                    ykonum = e.Y; // sağa sola çizgi yapılırsa xkonum ve ykonum belirleniyor.
+                }
+            }
+        }
+        private void aToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Eğer kalem sekmesine tıklanırsa çizme işleminin olması için durum değişkeni değeri değiştirildi.
+            durum = 1;
+            // Eğer kalem rengi değiştirilmek istenirse color dialog nesnemiz yaratıldı.
+            ColorDialog renk = new ColorDialog();
+            renk.ShowDialog();
+            kalem = new Pen(renk.Color,kalinlik);
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            //Kalemimizin kalınlıklarını ilgili sekmelere tıklanınca ayarlamış olduk.
+            kalinlik = 3;
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            //Kalemimizin kalınlıklarını ilgili sekmelere tıklanınca ayarlamış olduk.
+            kalinlik = 5;
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+            //Kalemimizin kalınlıklarını ilgili sekmelere tıklanınca ayarlamış olduk.
+            kalinlik = 8;
+        }
+
+        private void toolStripMenuItem7_Click(object sender, EventArgs e)
+        {
+            //Kalemimizin kalınlıklarını ilgili sekmelere tıklanınca ayarlamış olduk.
+            kalinlik = 10;
+        }
+
+        private void toolStripMenuItem8_Click(object sender, EventArgs e)
+        {
+            //Kalemimizin kalınlıklarını ilgili sekmelere tıklanınca ayarlamış olduk.
+            kalinlik = 20;
+        }
+
+        private void toolStripMenuItem9_Click(object sender, EventArgs e)
+        {
+            //Kalemimizin kalınlıklarını ilgili sekmelere tıklanınca ayarlamış olduk.
+            kalinlik = 30;
+        }
+    }
+}
